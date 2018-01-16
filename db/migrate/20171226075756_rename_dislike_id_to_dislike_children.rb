c3441815ab9d9990bcd33907d08ae48603f55124
kengo9990 <kengo9990@gmail.com>
2018-01-16 06:38:15 +0000

    dislike_id delete

--- db/migrate/20171226075756_rename_dislike_id_to_dislike_children.rb
+++ db/migrate/20171226075756_rename_dislike_id_to_dislike_children.rb
@@ -1,5 +1,6 @@
 class RenameDislikeIdToDislikeChildren < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :dislike_children, :dislike__id, :dislike_id
-  end
+  # いらない
+  # def change
+  #   rename_column :dislike_children, :dislike__id, :dislike_id
+  # end
 end
