7b828226ae322663aabf61a62385ad826768a0a6
kengo9990 <kengo9990@gmail.com>
2018-01-16 07:06:25 +0000

    seed_file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,45 @@
 ##### Colorsテーブル・テスト用データ
-# collor_names = %w(red blue yellow green orange gray black white )
-#     collor_names.each do |c_n|
-#         Color.create({name:c_n, image_color: Faker::Avatar.image})
-#     end
+collor_names = %w(red blue yellow green orange gray black white )
+    collor_names.each do |c_n|
+        Color.create({name:c_n, image_color: Faker::Avatar.image})
+    end
 
 ##### Stylesテーブル・テスト用データ(男)
-# style_names = %w(cool classic casual simple)
-#     style_names.each do |s_n|
-#         Style.create({name:s_n, sex: "1", image_style: Faker::Avatar.image})
-#     end
+style_names = %w(cool classic casual simple)
+    style_names.each do |s_n|
+        Style.create({name:s_n, sex: "1", image_style: Faker::Avatar.image})
+    end
 
 ##### Stylesテーブル・テスト用データ(女)
-# style_names = %w(cool classic casual simple)
-#     style_names.each do |s_n|
-#         Style.create({name:s_n, sex: "2", image_style: Faker::Avatar.image})
-#     end
+style_names = %w(cool classic casual simple)
+    style_names.each do |s_n|
+        Style.create({name:s_n, sex: "2", image_style: Faker::Avatar.image})
+    end
 
 
 # ##### Partternsテーブル・テスト用データ(男)
-# parttern_names = %w(striped dots check kamo animal logo )
-#     parttern_names.each do |p_n|
-#         Parttern.create({name:p_n, sex: "1", image_parttern: Faker::Avatar.image})
-#     end
+parttern_names = %w(striped dots check kamo animal logo )
+    parttern_names.each do |p_n|
+        Parttern.create({name:p_n, sex: "1", image_parttern: Faker::Avatar.image})
+    end
 
 ##### Partternsテーブル・テスト用データ(女)
-# parttern_names = %w(striped dots check hearts flowers stars)
-#     parttern_names.each do |p_n|
-#         Parttern.create({name:p_n, sex: "2", image_parttern: Faker::Avatar.image})
-#     end
+parttern_names = %w(striped dots check hearts flowers stars)
+    parttern_names.each do |p_n|
+        Parttern.create({name:p_n, sex: "2", image_parttern: Faker::Avatar.image})
+    end
 
 ##### Dislikesテーブル・テスト用データ(男)
-# dislike_names = %w(jeans harfpants sweaters bottonshirts downjacket parker)
-#     dislike_names.each do |d_n|
-#         Dislike.create({name:d_n, sex: "1", image_dislike: Faker::Avatar.image})
-#     end
+dislike_names = %w(jeans harfpants sweaters bottonshirts downjacket parker)
+    dislike_names.each do |d_n|
+        Dislike.create({name:d_n, sex: "1", image_dislike: Faker::Avatar.image})
+    end
 
 ##### Dislikesテーブル・テスト用データ(女)
-# dislike_names = %w(jeans harfpants dress skirts onepiece parker)
-#     dislike_names.each do |d_n|
-#         Dislike.create({name:d_n, sex: "2", image_dislike: Faker::Avatar.image})
-#     end
+dislike_names = %w(jeans harfpants dress skirts onepiece parker)
+    dislike_names.each do |d_n|
+        Dislike.create({name:d_n, sex: "2", image_dislike: Faker::Avatar.image})
+    end
 
 
 
