af2c1d60832577b29b872e997bebd11b42605c10
Kengo.Osawa <kengo.osawa@KengoOsawanoMacBook-puro.local>
2018-02-04 13:50:58 +0900

    ローカル、本番共にDB削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204020941) do
+ActiveRecord::Schema.define(version: 20180202121212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20180204020941) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "image"
+    t.index ["confirmation_token"], name: "index_customers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["name"], name: "index_customers_on_name", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
@@ -180,11 +181,6 @@ ActiveRecord::Schema.define(version: 20180204020941) do
     t.string "stripe_subscription_id"
     t.datetime "active_untill"
     t.text "image"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
